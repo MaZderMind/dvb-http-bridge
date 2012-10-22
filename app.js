eb59d09b7cfb7a19f3bfab24bacac77a5cfddb9a
MaZderMind <github@mazdermind.de>
2012-10-23 00:17:26 +0200

    load channels list

--- app.js
+++ app.js
@@ -3,32 +3,48 @@
  * Module dependencies.
  */
 
+console.log('loading modules');
 var express = require('express'),
-  routes = require('./routes'),
-  channels = require('./routes/channels'),
-  http = require('http'),
-  path = require('path');
+	expresshelpers = require('express-helpers');
+	routes = require('./routes'),
+	channels = require('./routes/channels'),
+	http = require('http'),
+	path = require('path'),
+	async = require('async');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(require('less-middleware')({ src: __dirname + '/public' }));
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	expresshelpers(app);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/channels', channels.list);
 
-app.listen(app.get('port'));
+// run init tasks 
+async.parallel([
+
+	channels.load,
+
+], function(err) {
+	if(err)
+		return console.error('error during init phase:', err);
+
+	console.log('listening');
+	app.listen(app.get('port'));
+})
--- routes/channels.js
+++ routes/channels.js
@@ -3,6 +3,37 @@
  * GET channels listing.
  */
 
+var
+	fs = require('fs'),
+	Iconv = require('iconv').Iconv,
+	nameConv = new Iconv('ISO-8859-1', 'UTF-8'),
+	channels = [];
+
+
+exports.load = function(cb){
+	console.log('loading channels list');
+	fs.readFile('/etc/channels.conf', function(err, data) {
+		if (err) throw err;
+
+		var n = 0;
+		nameConv.convert(data).toString().split("\n").forEach(function(line) {
+			line = line.split(':');
+			channels.push({idx: ++n, name: line[0], fav: false});
+		});
+
+		console.log('loaded', channels.length, 'channels')
+		cb();
+	});
+};
+
+exports.get = function() {
+	return channels;
+};
+
+exports.get = function() {
+	return channels;
+};
+
 exports.list = function(req, res){
-  res.send("respond with a list of channels");
+	res.send(channels);
 };
