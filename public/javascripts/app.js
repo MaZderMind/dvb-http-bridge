1501ef1687cc9e5ac694a9700397e4f8d4d53ee3
MaZderMind <github@mazdermind.de>
2012-10-26 21:10:32 +0200

    editfav button

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,4 +2,8 @@ $(function() {
 	$('#channelslist').listview('option', 'filterCallback', function(text, pattern) {
 		return pattern.toLowerCase() != text.substr(0, pattern.length).toLowerCase();
 	});
+
+	$('#editbtn').on('click', function(e) {
+		e.preventDefault();
+	});
 });
