5173071cf092819503a997786f308c01541d2963
MaZderMind <github@mazdermind.de>
2012-10-28 19:43:34 +0100

    toggle edit btn

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,24 @@
 $(function() {
+	var isEditingFavs = false;
+
 	$('#channelslist').listview('option', 'filterCallback', function(text, pattern) {
 		return pattern.toLowerCase() != text.substr(0, pattern.length).toLowerCase();
 	});
 
-	$('#editbtn').on('click', function(e) {
+	$('#editbtn').on('vclick', function(e) {
 		e.preventDefault();
+		e.stopPropagation();
+		isEditingFavs = $(this)
+			.toggleClass($.mobile.activeBtnClass)
+			.hasClass($.mobile.activeBtnClass);
+		
+		$('#channelslist .ui-icon')
+			.toggleClass('ui-icon-star', isEditingFavs);
+	});
+
+	$('#channelslist').bind('vclick', '.ui-btn', function(e) {
+		if(isEditingFavs) {
+			alert('add to fav');
+		}
 	});
 });
